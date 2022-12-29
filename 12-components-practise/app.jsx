0d14d150ee0482dc6385288a906b42a480550546
keshavsingla015 <97163964+keshavsingla015@users.noreply.github.com>
2022-12-29 14:31:48 +0530

    Create app.jsx

---
+++ 12-components-practise/app.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Heading from "./Heading";
+
+function App() {
+  return(
+  <Heading />);
+}
+
+export default App;
+
+
+// import React from "react";
+// import Heading from "./Heading";
+
+// function App() {
+//   return (
+//     <div>
+//       <Heading />
+//     </div>
+//   );
+// }
+
+// export default App;
